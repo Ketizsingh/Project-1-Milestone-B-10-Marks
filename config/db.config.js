78bf94dc79f0fe549b977580c27f0539b480e6cc
Ketizsingh <Gurpreettechnologies@gmail.com>
2020-12-06 21:08:27 -0500

    db folder + file created

---
+++ config/db.config.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql');
+require('dotenv').config();
+//new mysql db connection
+
+const dbConn = mysql.createConnection({
+
+    //will fix this in new update
+
+    // socketPath : process.env.SOCKET_PATH,
+    // host : process.env.HOST,
+    // port : process.env.PORT,
+    // user : process.env.USER,
+    // password : process.env.PASSWORD,
+    // database : process.env.DATABASE
+
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'gobabygo',
+    database: 'showel_angels'
+});
+
+dbConn.connect(function (error) {
+    if (error) throw error;
+    console.log('database connected successfully!!');
+})
+
+
+module.exports = dbConn;
